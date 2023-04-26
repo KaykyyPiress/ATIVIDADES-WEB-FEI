c4a93ae837ba5e3d90b6d6e5b1099128b05b74df
KaykyyPiress <kaykyypiress@gmail.com>
2023-04-26 16:30:46 -0300

    misturado com a aula

--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -1,13 +1,28 @@
 var http = require ('http');
-
 var express = require ('express');
+var bodyParser = require("body-parser");
+
 
 var app = express();
-
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 app.use(express.static('./public'));
 
+app.post("/logar", function(req, resp){
+    let login = req.body.login;
+    let senha = req.body.senha
+    resp.write("dados recebidos")
+    resp.write("Login: " + login)
+    reportError.write("Senha: " + senha)
+})
+
+app.post("/cadastra", function(req, resp){
+    var senha = req.body.senha;
+    var login = req.body.login
+    resp.render("resposta", {login,senha})
+})
+
+
 var server = http.createServer(app);
-
 server.listen(80);
-
 console.log("Servidor acho que foi...");
