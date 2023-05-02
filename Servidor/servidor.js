dc20421906f16de38ac5364b6534b7d6dc7cd173
KaykyyPiress <kaykyypiress@gmail.com>
2023-05-02 08:49:34 -0300

    servidor alterado

--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -1,28 +1,43 @@
-var http = require ('http');
-var express = require ('express');
-var bodyParser = require("body-parser");
-
+var http = require('http');
+var express = require('express');
+var bodyParser = require("body-parser")
 
 var app = express();
+app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
-app.use(express.static('./public'));
 
-app.post("/logar", function(req, resp){
+app.set("view engine", "ejs")
+app.set("views", "./views")
+
+app.get("/teste",function (req, resp){
+    console.log("Recebeu request");
+    resp.write("<h1> Resposta do /teste </h1>");
+    resp.end();
+
+});
+
+app.post("/cadastro",function (req, resp) {
+    let nome = req.body.nome;
     let login = req.body.login;
-    let senha = req.body.senha
-    resp.write("dados recebidos")
-    resp.write("Login: " + login)
-    reportError.write("Senha: " + senha)
-})
+    resp.write("<h1> Dados Recebidos</h1>");
+    resp.write(" Nome: "+ nome );
+    resp.write(" Login: "+ login );
+    resp.end();
 
-app.post("/cadastra", function(req, resp){
-    var senha = req.body.senha;
-    var login = req.body.login
-    resp.render("resposta", {login,senha})
-})
+});
 
+app.post("/login", function (req, resp){
+   var login = req.body.login;
+   var senha = req.body.senha;
+   resp.render("resposta", {login, senha})
+});
+
+
+app.get("/", function (req, resp){
+   resp.render("login")
+});
 
 var server = http.createServer(app);
 server.listen(80);
-console.log("Servidor acho que foi...");
+console.log("servidor rodando...");
