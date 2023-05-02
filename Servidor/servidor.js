b9d3d6044bcd6de7039d5eeabb676e841a39ed91
KaykyyPiress <kaykyypiress@gmail.com>
2023-05-02 08:07:11 -0300

    testes

--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -1,28 +1,51 @@
-var http = require ('http');
-var express = require ('express');
-var bodyParser = require("body-parser");
+const express = require('express');
+const app = express();
+const port = 3000;
 
+// Para lidar com dados de formulários codificados em URL e JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-var app = express();
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-app.use(express.static('./public'));
+// Definir rota para a página de registro
+app.get('/servidor', (req, res) => {
+  res.sendFile(__dirname + '/register.html');
+});
 
-app.post("/logar", function(req, resp){
-    let login = req.body.login;
-    let senha = req.body.senha
-    resp.write("dados recebidos")
-    resp.write("Login: " + login)
-    reportError.write("Senha: " + senha)
-})
+// Definir rota para a página de login
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/login.html');
+});
 
-app.post("/cadastra", function(req, resp){
-    var senha = req.body.senha;
-    var login = req.body.login
-    resp.render("resposta", {login,senha})
-})
+// Rota para lidar com o envio dos dados do formulário de registro
+app.post('/register', (req, res) => {
+  // Obter os dados do formulário do corpo da solicitação
+  const { username, password } = req.body;
+  
+  // Aqui você lidaria com o armazenamento dos dados do usuário no seu banco de dados ou qualquer outra lógica necessária
+  console.log(`Usuário registrado com sucesso! Nome de usuário: ${username}, senha: ${password}`);
+  
+  // Enviar uma resposta de sucesso ao usuário
+  res.render('resposta', { mensagem: 'Registro concluído com sucesso!' });
+});
 
+// Rota para lidar com o envio dos dados do formulário de login
+app.post('/login', (req, res) => {
+  // Obter os dados do formulário do corpo da solicitação
+  const { username, password } = req.body;
+  
+  // Aqui você lidaria com a validação dos dados do usuário no seu banco de dados ou qualquer outra lógica necessária
+  if (username === 'usuário' && password === 'senha') {
+    console.log(`Usuário logado com sucesso! Nome de usuário: ${username}, senha: ${password}`);
+    
+    // Enviar uma resposta de sucesso ao usuário
+    res.render('resposta', { mensagem: 'Login bem-sucedido!' });
+  } else {
+    // Enviar uma resposta de erro ao usuário
+    res.render('resposta', { mensagem: 'Nome de usuário ou senha incorretos!' });
+  }
+});
 
-var server = http.createServer(app);
-server.listen(80);
-console.log("Servidor acho que foi...");
+// Iniciar o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
